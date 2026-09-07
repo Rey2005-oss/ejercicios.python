8ab638e4c89d034c961bfe6e57e3bd0cad187011
Rey2005-oss <azuajereymar45@gmail.com>
2026-09-07 12:38:09 -0400

    Add files via upload
    Hola Ing tarde pero seguro

---
+++ automatizacion.py
@@ -0,0 +1,12 @@
+numero = 6
+
+if numero % 2 == 0:
+   print(f"{numero}es par")
+
+else:
+
+     print(f"{numero} es impar")
+
+
+
+
---
+++ calculadora.py
@@ -0,0 +1,11 @@
+# definir dos numeros
+
+a = 5
+b = 7
+
+#realizar suma
+suma = a + b
+
+#imprimir el resultado
+
+print("La suma es:", suma)
